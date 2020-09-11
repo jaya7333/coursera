c7a3ef7b23f1cc468ba1c00eb7ead1b50b3595b8
Jay Patel <pateljay2111996@gmail.com>
2020-09-11 16:34:27 +0530

    update

--- first_1.R
+++ first_1.R
@@ -5,3 +5,25 @@ myfunction <- function(x) {
 myfunction(100)
 ls()
 
+installed.packages()
+ls()
+
+rm=(list=ls())
+library(swirl)
+
+x <- 1
+print(x)
+
+new <- "Hello"
+new
+
+y <- 1:20
+y
+
+z<- 1/Inf
+z
+
+class(1)
+class(1L)
+
+length(new)
